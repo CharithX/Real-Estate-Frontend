809887e3c1491646adf65647c294b0c73f8071ee
CharithX <it20606374@my.sliit.lk>
2024-06-08 14:20:15 +0530

    card component done

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -8,25 +8,55 @@ function Filter() {
       </h1>
       <div className="top">
         <div className="item">
-          <label htmlFor="type">Type</label>
-          <select name="type" id="type">
-            <option value="buy">Buy</option>
-            <option value="rent">Rent</option>
-          </select>
+          <label htmlFor="city">Location</label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            placeholder="City Location"
+          />
         </div>
       </div>
       <div className="bottom">
         <div className="item">
-          <label htmlFor="city">Location</label>
-          <input type="text" id="city" placeholder="City Location" />
+          <label htmlFor="type">Type</label>
+          <select name="type" id="type">
+            <option value="">any</option>
+            <option value="buy">Buy</option>
+            <option value="rent">Rent</option>
+          </select>
         </div>
         <div className="item">
-          <label htmlFor="city">Location</label>
-          <input type="text" id="city" placeholder="City Location" />
+          <label htmlFor="property">Property</label>
+          <select name="property" id="property">
+            <option value="">any</option>
+            <option value="apartmant">Apartmant</option>
+            <option value="house">House</option>
+            <option value="condo">Condo</option>
+            <option value="land">Land</option>
+          </select>
         </div>
         <div className="item">
-          <label htmlFor="city">Location</label>
-          <input type="text" id="city" placeholder="City Location" />
+          <label htmlFor="minPrice">Min Price</label>
+          <input
+            type="number"
+            id="minPrice"
+            name="minPrice"
+            placeholder="any"
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="maxPrice">Max Price</label>
+          <input
+            type="number"
+            id="maxPrice"
+            name="maxPrice"
+            placeholder="any"
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="bedroom">Bedroom</label>
+          <input type="text" id="bedroom" name="bedroom" placeholder="any" />
         </div>
         <button>
           <img src="./search.png" alt="" />
