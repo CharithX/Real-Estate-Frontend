5bf4c9e69bc67a6985b9b702940e83b495d146a1
CharithX <it20606374@my.sliit.lk>
2024-06-09 01:53:50 +0530

    register functionality done

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,15 +1,53 @@
 import { Link } from "react-router-dom";
 import "./Register.scss";
+import axios from "axios";
+import { useState } from "react";
 
 function Register() {
+  const [error , setError] = useState("")
+
+
+  const handleSubmit = async (e) => {
+     e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    
+    console.log(username, email, password)
+    
+
+    try {
+      const res = await axios.post("http://localhost:8800/api/auth/register", {
+        username,
+        email,
+        password,  
+      });
+      console.log(res) 
+    }
+      catch (error) {
+      console.log(error)
+      //setError
+      }
+    
+    
+  }; 
+
   return (
-    <div className="register">
+    <div className="registerPage">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
-          <input name="username" type="text" placeholder="Username" />
-          <input name="email" type="text" placeholder="Email" />
-          <input name="password" type="password" placeholder="Password" />
+          <input name="username" type="text" placeholder="Username" required />
+          <input name="email" type="email" placeholder="Email" required />
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            required
+          />
           <button>Register</button>
           <Link to="/login">Do you have an account?</Link>
         </form>
